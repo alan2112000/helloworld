90b95ceed385529deb0547c58fab22640fcdb6fd
alanyu <alan21120000@gmail.com>
2012-12-12 03:09:22 +0800

    3rd function

--- helloworld/src/com/alanyu/git/helloworld.java
+++ helloworld/src/com/alanyu/git/helloworld.java
@@ -8,6 +8,8 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("this is branch test ");
+       System.out.println("this is another function");
+       System.out.println("this is 3rd function");
 	}
 
 }
