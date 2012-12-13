7c58cdceb02362f8de67f0a3fd2154216610da31
alanyu <alan21120000@gmail.com>
2012-12-13 11:00:38 +0800

    first commit after checkout

--- helloworld/src/com/alanyu/git/helloworld.java
+++ helloworld/src/com/alanyu/git/helloworld.java
@@ -8,6 +8,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("this is branch test ");
+       System.out.println("first commit after checkout ");
 	}
 
 }
